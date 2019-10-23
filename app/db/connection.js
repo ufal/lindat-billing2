801c0c8c3551d4b24843697cc46838e196514e15
Matyáš Kopp <kopp@ufal.mff.cuni.cz>
2019-10-23 14:40:49 +0200

    add sql query logging

--- app/db/connection.js
+++ app/db/connection.js
@@ -1,7 +1,13 @@
 const config = require('config');
 const promise = require('bluebird');
 const logger = require('../logger');
-const pgp = require('pg-promise')({promiseLib: promise});
+const pgp = require('pg-promise')(
+    {
+        promiseLib: promise,
+        query: function (e) {
+            logger.debug('QUERY:', pgp.as.format(e.query,e.params));
+        }
+});
 
 // Database connection details;
 let db = pgp(config.db);
