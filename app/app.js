0021237ad45c9e4f6940318b93037fb235715608
Matyáš Kopp <kopp@ufal.mff.cuni.cz>
2019-12-18 09:46:05 +0100

    add method-override

--- app/app.js
+++ app/app.js
@@ -7,6 +7,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const session = require('express-session')
 const cookeParser =  require('cookie-parser');
+const methodOverride = require('method-override');
 const logger = require('./logger');
 const config = require('config');
 const routes = require('./routes');
@@ -27,6 +28,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
+
+app.use(methodOverride('_method'));
 
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
