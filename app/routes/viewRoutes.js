e032742f026d10c501aed25dfed3d13097b95616
Matyáš Kopp <kopp@ufal.mff.cuni.cz>
2020-10-26 13:39:23 +0100

    use current date in dashboard preview

--- app/routes/viewRoutes.js
+++ app/routes/viewRoutes.js
@@ -206,16 +206,16 @@ router.post('/add-endpoint', function (req, res, next) {
 
 async function getAdminDashboard(user, res){
   const len = 21;
-  const last_date = '2019-01-15';
+  const last_date = (new Date).toISOString().slice(0,10);
   const weekly = await adminController.getWeeklyCountsByService(last_date, len);
-  res.render('dashboard', {user: user, servicecounts: weekly, period_length: len, date: Date.parse(last_date), initialview: '2019-01', admin_dashboard_active: true, filteruser: 'all', datalines: ["users","requests"]});
+  res.render('dashboard', {user: user, servicecounts: weekly, period_length: len, date: Date.parse(last_date), initialview: (new Date).toISOString().slice(0,7), admin_dashboard_active: true, filteruser: 'all', datalines: ["users","requests"]});
 }
 
 async function getUserDashboard(user, res){
   const len = 21;
-  const last_date = '2019-01-15';
+  const last_date = (new Date).toISOString().slice(0,10);
   const weekly = await userController.getWeeklyCountsByService(user.user_id, last_date , len);
-  res.render('dashboard', {user: user, servicecounts: weekly, period_length: len, date: last_date, initialview: '2019-01', user_dashboard_active: true, filteruser: user.user_id, datalines: ["prices","requests"]});
+  res.render('dashboard', {user: user, servicecounts: weekly, period_length: len, date: last_date, initialview: (new Date).toISOString().slice(0,7), user_dashboard_active: true, filteruser: user.user_id, datalines: ["prices","requests"]});
 }
 
 module.exports = router;
