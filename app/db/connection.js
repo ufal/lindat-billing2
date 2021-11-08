a81a9046bee7e26490671f98d577ce5f4633d99d
Matyáš Kopp <kopp@ufal.mff.cuni.cz>
2021-11-08 10:54:57 +0100

    set connection timeout on testConnection

--- app/db/connection.js
+++ app/db/connection.js
@@ -15,18 +15,14 @@ pgp.pg.types.setTypeParser(types.builtins.INT8, parseInt);
 // Database connection details;
 let db = pgp(config.db);
 logger.info('Connecting to database [%s:%s]: DB: %s, USER %s',config.db.host, config.db.port, config.db.database, config.db.user);
-db.connect()
-    .then(obj => {
-      logger.trace();
-      obj.done(); // release the connection
-    })
-    .catch(error => {
-      logger.trace();
-      logger.error('Error connecting to database [%s:%s]:', config.db.host, config.db.port, error);
-});
 
 async function testConnection() {
-    const c = await db.connect(); // try to connect
+    const c = await db.connect()
+      .timeout(3000)
+      .catch(error => {
+        logger.trace();
+        logger.error('Error connecting to database [%s:%s]:', config.db.host, config.db.port, error);
+      }); // try to connect
     c.done(); // success, release connection
     logger.info("database connection test: OK");
     return c.client.serverVersion; // return server version
