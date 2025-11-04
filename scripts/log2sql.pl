1306211350a3a414efc18b4f1297fac1232a0760
Matyáš Kopp <kopp@ufal.mff.cuni.cz>
2025-11-04 12:55:01 +0100

    print data instead of stashing them in variable

--- scripts/log2sql.pl
+++ scripts/log2sql.pl
@@ -198,14 +198,32 @@ for my $log_file_path (@logfiles) {
   close LOG;
   print STDERR "INFO: Finished processing $log_file_path\n";
 
+
+
+  # writting sql commands
+  open SQL, ">".File::Spec->catfile($outdir,$sql_file) or die "Could not open $sql_file: $!";
+  print SQL " -- $log_file sql dump
+DO \$\$ BEGIN RAISE NOTICE 'STARTED--------------[\%]----------------', NOW(); END; \$\$;
+ALTER TABLE log_file_entries DISABLE TRIGGER log_files_lines_read;
+ALTER TABLE log_file_entries DISABLE TRIGGER log_files_lines_read_aggr;
+
+
+INSERT
+INTO log_files(file_id, file_name, first_line_checksum, last_read_line_checksum, lines_read, lines_valid, tail)
+VALUES($file_id,'$log_file','$first_line_checksum', '$last_read_line_checksum', $lines_read, $lines_valid,FALSE);
+";
+
+
   print STDERR "INFO: Start month aggregation on $log_file_path\n";
   my $act_month = $prev_date; $act_month =~ s/^../01/;
-  my $month_aggr_sql = add_aggregate_data_string($aggr_ip_data,$act_month,'month');
-  push @print_sql,"$month_aggr_sql";
+  
+  #my $month_aggr_sql = add_aggregate_data_string($aggr_ip_data,$act_month,'month');
+  #push @print_sql,"$month_aggr_sql";
+  print_aggregate_data(*SQL, $aggr_ip_data,$act_month,'month')
   print STDERR "INFO: Finished month aggregation on $log_file_path\n";
 
   # generating endpoints data
-  push @print_sql," -- aggregating endpoints data
+  print SQL " -- aggregating endpoints data
 -- hour + day aggregations
 INSERT INTO log_aggr
 (
@@ -331,21 +349,7 @@ SELECT
             la.token_id;
 
 ";
-
-
-  # writting sql commands
-  open SQL, ">".File::Spec->catfile($outdir,$sql_file) or die "Could not open $sql_file: $!";
-  print SQL " -- $log_file sql dump
-DO \$\$ BEGIN RAISE NOTICE 'STARTED--------------[\%]----------------', NOW(); END; \$\$;
-ALTER TABLE log_file_entries DISABLE TRIGGER log_files_lines_read;
-ALTER TABLE log_file_entries DISABLE TRIGGER log_files_lines_read_aggr;
-
-
-INSERT
-INTO log_files(file_id, file_name, first_line_checksum, last_read_line_checksum, lines_read, lines_valid, tail)
-VALUES($file_id,'$log_file','$first_line_checksum', '$last_read_line_checksum', $lines_read, $lines_valid,FALSE);
-";
-  print SQL "$_\n" for @print_sql;
+# ================================================
 
   print SQL "
 ALTER TABLE log_file_entries ENABLE TRIGGER log_files_lines_read;
@@ -440,10 +444,9 @@ sub aggregate_data{
   }
 }
 
-sub add_aggregate_data_string{
-  my ($aggr_ip_data, $start_time, $period) = @_;
+sub print_aggregate_data{
+  my ($fh, $aggr_ip_data, $start_time, $period) = @_;
   return unless $start_time;
-  my $str = '';
   my $end_time = $strp->parse_datetime($start_time);
   $end_time->add("${period}s" => 1);
   $end_time = $end_time->strftime('%d/%b/%Y:%H:%M:%S %z');
@@ -455,9 +458,9 @@ sub add_aggregate_data_string{
                       ."AND token_used=%s "
                       ."AND %s "
                       ."AND %s ", $start_time, $period, $token_used?'TRUE':'FALSE', sql_equal_statement('ip',$ip), sql_equal_statement('service_id', $service));
-        $str .= sprintf("UPDATE log_ip_aggr SET cnt_requests = cnt_requests + %d, cnt_units = cnt_units + %d, cnt_body_bytes_sent = cnt_body_bytes_sent + %d "
+        print $fh sprintf("UPDATE log_ip_aggr SET cnt_requests = cnt_requests + %d, cnt_units = cnt_units + %d, cnt_body_bytes_sent = cnt_body_bytes_sent + %d "
                       ."%s;\n", @{$aggr_ip_data->{$period}->{$ip}->{$token_used}->{$service}}, $restrictions);
-        $str .= sprintf("INSERT INTO log_ip_aggr(period_start_date, period_end_date, period_level, ip, service_id, cnt_requests, cnt_units, cnt_body_bytes_sent, token_used)"
+        print $fh sprintf("INSERT INTO log_ip_aggr(period_start_date, period_end_date, period_level, ip, service_id, cnt_requests, cnt_units, cnt_body_bytes_sent, token_used)"
                       ."SELECT '%s', '%s', '%s', "
                       ."%s, %s, %d, %d, %d, %s "
                       ."WHERE NOT EXISTS (SELECT 1 FROM log_ip_aggr %s);\n",
@@ -470,7 +473,6 @@ sub add_aggregate_data_string{
       }
     }
   }
-  return $str;
 }
 
 sub sql_equal_statement{
